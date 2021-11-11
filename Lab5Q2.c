3027b44e5f75a466153c4b9c49c9a5ff80b4a2e5
CasualPM28 <60796502+CasualPM28@users.noreply.github.com>
2021-11-11 17:42:20 -0500

    Update Lab5Q2.c

--- Lab5Q2.c
+++ Lab5Q2.c
@@ -6,10 +6,10 @@ void main(void)
     P1DIR |= BIT6;             // set the direction to the red LED
     P1OUT = 0x00;              // set the initial output to be 0x00
     P1SEL |= BIT6;             // select output signal on red LED
-    TACCR0 = 500000 - 1;       // set TACCR0 to 500ms, or 500,000 microseconds
+    TACCR0 = 62499;       // set TACCR0 to 62499, or 500,000 microseconds in TACCR0
     TACCTL1 = OUTMOD_7;        // set to mode 7 for the output signal
-    TACCR1 = 1000-1;           // PWM Duty Cycle of 10%
-    TACTL = TASSEL_1 + MC_1;   // use ACLK and up mode
+    TACCR1 = 6250;           // PWM Duty Cycle of 10% (10% of 62499)
+    TACTL = TASSEL_2 + MC_1 + ID_3;   // use SMCLK and up mode
 
     while(1){
         P1OUT ^= BIT6;          // set BIT6 toggle
